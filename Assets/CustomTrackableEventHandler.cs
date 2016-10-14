46113f48040f2a524747f12d5ac606341ae5ec0b
Vinícius Guerra Cardoso <dev.viniciusguerra@gmail.com>
2016-10-14 20:48:23 -0300

    Initial Commit

---
+++ Assets/CustomTrackableEventHandler.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+using Vuforia;
+
+
+public class CustomTrackableEventHandler : MonoBehaviour,
+                                            ITrackableEventHandler
+{
+    private TrackableBehaviour mTrackableBehaviour;
+    public CustomEventString OnFoundTrack;
+    public CustomEventString OnLostTrack;
+	TrackableBehaviour.Status oldStatus = TrackableBehaviour.Status.NOT_FOUND;
+
+    void Start()
+    {
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+        {
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+        }
+    }
+
+    public void OnTrackableStateChanged(
+                                    TrackableBehaviour.Status previousStatus,
+                                    TrackableBehaviour.Status newStatus)
+    {
+		
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            OnTrackingFound();
+        }
+		else if(oldStatus == TrackableBehaviour.Status.DETECTED ||
+			oldStatus == TrackableBehaviour.Status.TRACKED ||
+			oldStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            OnTrackingLost();
+        }
+		oldStatus = newStatus;
+    }
+
+    private void OnTrackingFound()
+    {
+        Renderer[] rendererComponents = GetComponentsInChildren<Renderer>(true);
+        Collider[] colliderComponents = GetComponentsInChildren<Collider>(true);
+
+        // Enable rendering:
+        foreach (Renderer component in rendererComponents)
+        {
+            component.enabled = true;
+        }
+
+        // Enable colliders:
+        foreach (Collider component in colliderComponents)
+        {
+            component.enabled = true;
+        }
+
+        Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " found");
+        OnFoundTrack.Invoke(mTrackableBehaviour.TrackableName);
+    }
+
+
+    private void OnTrackingLost()
+    {
+        Renderer[] rendererComponents = GetComponentsInChildren<Renderer>(true);
+        Collider[] colliderComponents = GetComponentsInChildren<Collider>(true);
+
+        // Disable rendering:
+        foreach (Renderer component in rendererComponents)
+        {
+            component.enabled = false;
+        }
+
+        // Disable colliders:
+        foreach (Collider component in colliderComponents)
+        {
+            component.enabled = false;
+        }
+
+        Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " lost");
+        OnLostTrack.Invoke(mTrackableBehaviour.TrackableName);
+    }
+
+
+}
+
+[Serializable]
+public class CustomEventString : UnityEvent<string>
+{
+
+}
---
+++ Assets/legenda.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+		
+public class legenda : MonoBehaviour {
+	public Text legendaexibida, iniciarfilme;
+	public string [] bancolegenda;
+	public float relogio, temporizador;
+	public bool iniciar = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!iniciar)
+			return;
+
+		Debug.Log ("aaa");
+			relogio += Time.deltaTime;
+
+			if (relogio >= 0.5f && relogio <= 0.1f) {
+				legendaexibida.text = bancolegenda [0];
+
+			}
+
+			if (relogio >= 1.1f && relogio <= 4f) {
+				legendaexibida.text = bancolegenda [1];
+
+			}
+			if (relogio >= 5f && relogio <= 9f) {
+			legendaexibida.text = bancolegenda [2];
+
+			}
+			if (relogio >= 10f && relogio <= 16f) {
+				legendaexibida.text = bancolegenda [3];
+
+			}
+			if (relogio >= 17f && relogio <= 21f) {
+				legendaexibida.text = bancolegenda [4];
+			}
+			if (relogio >= 22f && relogio <= 23f) {
+				legendaexibida.text = bancolegenda [5];
+			}
+			if (relogio >= 23.2f && relogio <= 28f) {
+			legendaexibida.text = bancolegenda [6];
+			}
+			if (relogio >= 29f && relogio <= 31f) {
+				legendaexibida.text = bancolegenda [7];
+			}
+			if (relogio >= 32f && relogio <= 36f) {
+			legendaexibida.text = bancolegenda [8];
+			}
+			if (relogio >= 36.1f && relogio <= 40f) {
+			legendaexibida.text = bancolegenda [9];
+			}
+		if (relogio >= 42.1f && relogio <= 43f) {
+			legendaexibida.text = "";
+		}
+	
+	}
+	public void Achou()
+	{	
+		iniciarfilme.text = "Seu Filme vai começar";
+		iniciar = false;
+
+
+	}
+	public void Sumiu()
+	{
+		iniciar = true;	
+		iniciarfilme.text = "";
+
+		
+	}
+}
